abae77b5ae4ad2dfa729ebbf19035fdfd4164d9d
1622038-PC\1622038 <tatsuya.syobobon@gmail.com>
2018-01-27 18:34:38 +0900

    セーブシーン、ロードシーン、およびそれらのスクリプトの追加

---
+++ Assets/02Scripts/SaveManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SaveManagerScript : MonoBehaviour {
+	public static string SCENE_NAME = "SceneName";
+	public static int gra;
+	string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		//gra = GameManagerScript.gra;						//SceneGameからgraの値を持ってくる
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void PushSaveButton(int saveNo){					//押されたセーブボタンのスクリプト＆ボタンの番号読み込み
+		Debug.Log (saveNo);											//選択したロードボタンをログで表示
+		PlayerPrefs.SetInt ("data" + saveNo + "gravity", gra);		//セーブ内容
+		PlayerPrefs.Save ();										//セーブ
+	}
+
+	public void PushBackButton(){							//戻るボタンを押したときのスクリプト
+		sceneName = PlayerPrefs.GetString(SCENE_NAME);
+		SceneManager.LoadScene (sceneName);
+	}
+}
