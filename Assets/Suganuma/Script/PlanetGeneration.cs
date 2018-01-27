3388ad0124a5795b4abe2489d92fdb60ca8c4b82
T.Suganuma <m01151898b@edu.teu.ac.jp>
2018-01-27 19:35:41 +0900

    初期設定画面の追加 テクスチャ無し、ゲーム画面への切り替えなし、モデル切り替えなし、音なし

---
+++ Assets/Suganuma/Script/PlanetGeneration.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGeneration : MonoBehaviour {
+
+    private bool moveStart;
+    [SerializeField] GameObject lookPoint;
+    private float deltaTimeWait = 1;
+
+    [SerializeField] Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        
+    }
+
+    void MoveStart()
+    {
+        moveStart = true;
+        //anim.SetBool("GenerationStart", true);
+    }
+
+    void Move()
+    {
+        if (moveStart == true)
+        {
+            deltaTimeWait -= Time.deltaTime;
+            if(deltaTimeWait <= 0)
+            {
+                //transform.Translate(1 * Time.deltaTime, 0, 2 * Time.deltaTime);
+            }            
+        }
+    }
+}
