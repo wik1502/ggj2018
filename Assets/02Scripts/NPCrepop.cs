fcd6cb4d75b7c65bf2b78c244e41d2133efc37b7
T.Suganuma <m01151898b@edu.teu.ac.jp>
2018-01-28 03:27:15 +0900

    NPCの属性データを格納（グラフィック未反映）

--- Assets/02Scripts/NPCrepop.cs
+++ Assets/02Scripts/NPCrepop.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class NPCrepop : MonoBehaviour {
 
+
     [SerializeField] private GameObject npc;　//ポップさせるNPC
     private Vector3 popPos;
+    int debugkey = -1;
 
     // Use this for initialization
     void Start()
@@ -16,13 +18,105 @@ public class NPCrepop : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            debugkey = 1;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            debugkey = 2;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            debugkey = 3;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            debugkey = 4;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha5))
+        {
+            debugkey = 5;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha6))
+        {
+            debugkey = 6;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha7))
+        {
+            debugkey = 7;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha8))
+        {
+            debugkey = 8;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha9))
+        {
+            debugkey = 9;
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha0))
+        {
+            debugkey = 0;
+        }
     }
 
     void Repop()
     {
         //ポップ時の音をここで流す
-        popPos = new Vector3(Random.Range(-14, 14), Random.Range(-5, 5), 0);
-        Instantiate(npc, popPos, transform.rotation);
+        popPos = new Vector3((Random.Range(0, 10) + 5) * (Random.Range(0, 2) * 2 - 1), (Random.Range(0, 3) + 3) * (Random.Range(0, 2) * 2 - 1), 0);
+        GameObject generatedNPC = Instantiate(npc, popPos, transform.rotation);
+        int mParam = 0;
+        int sParam = 0;
+        switch (debugkey)
+        {
+            case 0:
+                mParam = 0;
+                sParam = 4;
+                break;
+            case 1:
+                mParam = 1;
+                sParam = 4;
+                break;
+            case 2:
+                mParam = 2;
+                sParam = 4;
+                break;
+            case 3:
+                mParam = 3;
+                sParam = 4;
+                break;
+            case 4:
+                mParam = 4;
+                sParam = 4;
+                break;
+            case 5:
+                mParam = 0;
+                sParam = 0;
+                break;
+            case 6:
+                mParam = 0;
+                sParam = 1;
+                break;
+            case 7:
+                mParam = 0;
+                sParam = 2;
+                break;
+            case 8:
+                mParam = 0;
+                sParam = 3;
+                break;
+            case 9:
+                mParam = 0;
+                sParam = 0;
+                break;
+            default:
+                mParam = Random.Range(0, 5);
+                sParam = Random.Range(0, 4);
+                break;
+        }
+
+        debugkey = -1;
+        generatedNPC.GetComponent<NpcParameter>().SetParam(mParam, sParam);
+        SoundManager.Instance.PlaySE(1);                        //1=決定音
     }
 }
--- Assets/02Scripts/NpcParameter.cs
+++ Assets/02Scripts/NpcParameter.cs
@@ -6,15 +6,38 @@ using UnityEngine;
 //ただし衝突時の加減算で全パラメータ配列を計算しているので、パラメータ数を減らしたりするとそこでエラーが出ます。
 //そこも書き換えちゃうか山本に言ってください。
 public class NpcParameter : MonoBehaviour {
-    
-    public int[] mainParameter;   //NPCのメインパラメータ
-    public int[] subParameter;    //NPCのサブパラメータ
+    private enum MAIN_PARA_ID
+    {
+        normal = 0,
+        tempe,
+        air,
+        grav,
+        mass,
+        MAXID
+    };
+
+    private enum SAB_PARA_ID
+    {
+        water = 0,
+        elec,
+        pois,
+        metal,
+        MAXID
+    };
+
+    [SerializeField] GameObject[] iconPrefab;
+    private MAIN_PARA_ID mainParam;
+    private SAB_PARA_ID subParam;
+
+    //public int[] mainParameter;   //NPCのメインパラメータ
+    //public int[] subParameter;    //NPCのサブパラメータ
     GameMainSystem gameSystem;
-    
-    void Start () {
+
+    void Start() {
         gameSystem = GameObject.Find("GameSystem").GetComponent<GameMainSystem>();
-        mainParameter = new int[gameSystem.mainParameterNum];   //NPCのメインパラメータの配列確保
-        subParameter = new int[gameSystem.subParameterNum];     //NPCのメインパラメータの配列確保
+/*
+        mainParameter = new int[(int)MAIN_PARA_ID.MAXID];   //NPCのメインパラメータの配列確保
+        subParameter = new int[SAB_PARA_ID.MAXID];     //NPCのメインパラメータの配列確保
 
         //NPCのメインパラメータのランダム生成
         for (int i = 0; i < mainParameter.Length; i++)
@@ -29,11 +52,27 @@ public class NpcParameter : MonoBehaviour {
             subParameter[i] = Random.Range(0, gameSystem.limitParameter);
         }
         //Debug.Log("NPC subParameter0：" + subParameter[0]);
+ */
     }
-	
-	void Update () {
-		
-	}
 
-    
+    void Update() {
+
+    }
+
+    public void SetParam(int mParam, int sParam) {
+        mainParam = (MAIN_PARA_ID)mParam;
+        subParam = (SAB_PARA_ID)sParam;
+
+        string[] mpStr = { "normal", "tempe", "air", "grav", "mass" };
+        string[] spStr = { "water", "elec", "pois", "metal", "subnormal???" };
+
+        Debug.Log((int)mainParam + ":" + (int)subParam);
+        Debug.Log(mpStr[(int)mainParam] + ":" + spStr[(int)subParam]);
+
+
+        GameObject a = Instantiate(iconPrefab[(int)subParam], gameObject.transform);
+        a.transform.parent = gameObject.transform;
+        a.transform.position += new Vector3(0.5f, 0.5f, -0.5f);
+    }
+
 }
