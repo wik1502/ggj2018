e4f265dbaa6bbdd5ea9285a0682b9fe302cdbeb4
T.Suganuma <m01151898b@edu.teu.ac.jp>
2018-01-28 00:07:01 +0900

    NPCが消えたら新しいNPCをポップする機能の追加 生まれてくるNPCの属性などはまだついてません

---
+++ Assets/02Scripts/NPCrepop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCrepop : MonoBehaviour {
+
+    [SerializeField] private GameObject npc;　//ポップさせるNPC
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Repop()
+    {
+        //ポップ時の音をここで流す
+        Instantiate(npc, transform.position, transform.rotation);
+    }
+}
